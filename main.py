c40300f3d4049f8d0f3501f63e76bc4949ae391e
ChehaYuri <sacra1364069@gmail.com>
2023-04-16 08:46:23 +0300

    задача 2

--- main.py
+++ main.py
@@ -12,3 +12,17 @@
 #             return False
 # total = authorization(username, password)
 # print(total)
+
+def author_info(**kwargs):
+    full_name = kwargs.get('full_name', '')
+    birth_date = kwargs.get('birth_date', '')
+    death_date = kwargs.get('death_date', '')
+    description = kwargs.get('description', '')
+
+    if full_name:
+        initials = ''.join([name[0] + '.' for name in full_name.split()])
+        print(f'{initials} {full_name.split()[-1]} ({birth_date} - {death_date}) {description}')
+    else:
+        print('Информация об авторе отсутствует')
+author_info(full_name='Иван Сергеевич Тургенев', birth_date='28 октября 1818', death_date='22 августа 1883', description='русский писатель и общественный деятель')
+print(author_info())
